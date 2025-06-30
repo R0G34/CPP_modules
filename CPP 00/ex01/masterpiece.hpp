50f2f1fad86b2341383a96a59f6cd4fa64909c08
ALBERT BAUSA VOZNIY <zcb00268@tc2.tech>
2025-06-30 11:13:30 +0200

    fixed errors, working

---
+++ CPP 00/ex01/masterpiece.hpp
@@ -0,0 +1,17 @@
+#ifndef MASTERPIECE_H
+#define MASTERPIECE_H
+
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <unistd.h>
+#include <sctdlib>
+#include <iomanip>
+#include <cstdio>
+
+typedef	std::string str;
+
+#include "Contact.hpp"
+#include "PhoneBook.hpp"
+
+#endif
